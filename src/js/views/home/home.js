6784b0ec41fbce58c73e3ff0b042b78b8d72afc3
Samir Benzada <samirbenzada@gmail.com>
2020-03-18 14:54:42 -0400

    update

--- src/js/views/home/home.js
+++ src/js/views/home/home.js
@@ -22,91 +22,91 @@ const Home = () => {
                 </div>
             </div>
 
-            <div class="banner-btm">
-			<div class="container">
-				<div class="banner-bottom-main">
-					<div class="col-md-4 banner-btmg1">
+            <div class={styles.bannerServices}>
+                <div class="container">
+                    <div class="banner-bottom-main">
+                        <div class="col-md-4 banner-btmg1">
 
-						<div class="form-text">
-							<h3>Looking for a Handyman?</h3>
-							<p>We solve your Home repair needs!</p>
-							<img src="images/f1.png" alt="" />
-						</div>
-						<form action="#" method="post" class="banner_form">
-							<div class="sec-left">
-								<label class="contact-form-text">Name</label>
-								<input placeholder="your name " name="first name" type="text" required="" />
-							</div>
-							<div class="sec-right">
-								<label class="contact-form-text">Email</label>
-								<input placeholder="xxx@xx.com " name="first name" type="email" required="" />
-							</div>
-							<div class="sec-left">
-								<label class="contact-form-text">Mobile no.</label>
-								<input placeholder=" XXXXXX" name="first name" type="text" required="" />
-							</div>
-							<div class="form-tx">
-								<label class="contact-form-text">Address</label>
-								<textarea placeholder="your address" required=""></textarea>
-							</div>
-							<div class="form-select sec-right">
-								<label class="contact-form-text">Select Service</label>
-								<select>
-									<option value="0">---- SELECT ----</option>
-									<option value="1">Electrical Service</option>
-									<option value="2">Plumbing Service</option>
-									<option value="3">Painting</option>
-									<option value="4">Carpentry</option>
-									<option value="5">Gardening</option>
-									<option value="6">Other</option>
-								</select>
-							</div>
-							<input type="submit" value="Submit" />
-						</form>
-					</div>
-					<div class="col-md-8 banner-btm-grid2">
-						<div class="col-md-4 banner-grid2">
-							<div class="banner-subg1">
-								<h3>maintenance</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
-								<span class="fa fa-cog" aria-hidden="true"></span>
-								<div class="read-btn">
-									<a href="about.html">view more</a>
-								</div>
-							</div>
-							<div class="banner-subg1">
-								<h3>cleaning</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
-								<span class="fa fa-check-square-o" aria-hidden="true"></span>
-								<div class="read-btn">
-									<a href="about.html">view more</a>
-								</div>
-							</div>
+                            <div class="form-text">
+                                <h3>Looking for a Handyman?</h3>
+                                <p>We solve your Home repair needs!</p>
+                                <img src="images/f1.png" alt="" />
+                            </div>
+                            <form action="#" method="post" class="banner_form">
+                                <div class="sec-left">
+                                    <label class="contact-form-text">Name</label>
+                                    <input placeholder="your name " name="first name" type="text" required="" />
+                                </div>
+                                <div class="sec-right">
+                                    <label class="contact-form-text">Email</label>
+                                    <input placeholder="xxx@xx.com " name="first name" type="email" required="" />
+                                </div>
+                                <div class="sec-left">
+                                    <label class="contact-form-text">Mobile no.</label>
+                                    <input placeholder=" XXXXXX" name="first name" type="text" required="" />
+                                </div>
+                                <div class="form-tx">
+                                    <label class="contact-form-text">Address</label>
+                                    <textarea placeholder="your address" required=""></textarea>
+                                </div>
+                                <div class="form-select sec-right">
+                                    <label class="contact-form-text">Select Service</label>
+                                    <select>
+                                        <option value="0">---- SELECT ----</option>
+                                        <option value="1">Electrical Service</option>
+                                        <option value="2">Plumbing Service</option>
+                                        <option value="3">Painting</option>
+                                        <option value="4">Carpentry</option>
+                                        <option value="5">Gardening</option>
+                                        <option value="6">Other</option>
+                                    </select>
+                                </div>
+                                <input type="submit" value="Submit" />
+                            </form>
+                        </div>
+                        <div class="col-md-8 banner-btm-grid2">
+                            <div class="col-md-4 banner-grid2">
+                                <div class="banner-subg1">
+                                    <h3>maintenance</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
+                                    <span class="fa fa-cog" aria-hidden="true"></span>
+                                    <div class="read-btn">
+                                        <a href="about.html">view more</a>
+                                    </div>
+                                </div>
+                                <div class="banner-subg1">
+                                    <h3>cleaning</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
+                                    <span class="fa fa-check-square-o" aria-hidden="true"></span>
+                                    <div class="read-btn">
+                                        <a href="about.html">view more</a>
+                                    </div>
+                                </div>
 
-						</div>
-						<div class="col-md-4 banner-grid2">
-							<div class="banner-subg1">
-								<h3>repair</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
-								<span class="fa fa-yelp" aria-hidden="true"></span>
-								<div class="read-btn">
-									<a href="about.html">view more</a>
-								</div>
-							</div>
-							<div class="banner-subg1">
-								<h3>improvement</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
-								<span class="fa fa-gg" aria-hidden="true"></span>
-								<div class="read-btn">
-									<a href="about.html">view more</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
+                            </div>
+                            <div class="col-md-4 banner-grid2">
+                                <div class="banner-subg1">
+                                    <h3>repair</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
+                                    <span class="fa fa-yelp" aria-hidden="true"></span>
+                                    <div class="read-btn">
+                                        <a href="about.html">view more</a>
+                                    </div>
+                                </div>
+                                <div class="banner-subg1">
+                                    <h3>improvement</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
+                                    <span class="fa fa-gg" aria-hidden="true"></span>
+                                    <div class="read-btn">
+                                        <a href="about.html">view more</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+		    </div>
 
         </div>
     );

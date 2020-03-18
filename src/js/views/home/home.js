40c227ccf6d31e44a13d322ce20c51a34bd73078
Samir Benzada <samirbenzada@gmail.com>
2020-03-18 15:06:28 -0400

    update

--- src/js/views/home/home.js
+++ src/js/views/home/home.js
@@ -25,84 +25,72 @@ const Home = () => {
             <div className={styles.bannerServices}>
                 <div className="container">
                     <div className="row">
-                        <div className={["col-md-4", styles.bannerBox1].join(' ')}>
-                            <div className="form-text">
-                                <h3>Looking for a Handyman?</h3>
-                                <p>We solve your Home repair needs!</p>
-                                <img src="images/f1.png" alt="" />
-                            </div>
-                            <form action="#" method="post" className="banner_form">
-                                <div className="sec-left">
-                                    <label className="contact-form-text">Name</label>
-                                    <input placeholder="your name " name="first name" type="text" required="" />
-                                </div>
-                                <div className="sec-right">
-                                    <label className="contact-form-text">Email</label>
-                                    <input placeholder="xxx@xx.com " name="first name" type="email" required="" />
-                                </div>
-                                <div className="sec-left">
-                                    <label className="contact-form-text">Mobile no.</label>
-                                    <input placeholder=" XXXXXX" name="first name" type="text" required="" />
-                                </div>
-                                <div className="form-tx">
-                                    <label className="contact-form-text">Address</label>
-                                    <textarea placeholder="your address" required=""></textarea>
-                                </div>
-                                <div className="form-select sec-right">
-                                    <label className="contact-form-text">Select Service</label>
-                                    <select>
-                                        <option value="0">---- SELECT ----</option>
-                                        <option value="1">Electrical Service</option>
-                                        <option value="2">Plumbing Service</option>
-                                        <option value="3">Painting</option>
-                                        <option value="4">Carpentry</option>
-                                        <option value="5">Gardening</option>
-                                        <option value="6">Other</option>
-                                    </select>
-                                </div>
-                                <input type="submit" value="Submit" />
-                            </form>
-                        </div>
-                        <div className="col-md-8 banner-btm-grid2">
-                            <div className="col-md-4 banner-grid2">
-                                <div className="banner-subg1">
-                                    <h3>maintenance</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
-                                    <span className="fa fa-cog" aria-hidden="true"></span>
-                                    <div className="read-btn">
-                                        <a href="about.html">view more</a>
-                                    </div>
-                                </div>
-                                <div className="banner-subg1">
-                                    <h3>cleaning</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
-                                    <span className="fa fa-check-square-o" aria-hidden="true"></span>
-                                    <div className="read-btn">
-                                        <a href="about.html">view more</a>
-                                    </div>
-                                </div>
 
-                            </div>
-                            <div className="col-md-4 banner-grid2">
-                                <div className="banner-subg1">
-                                    <h3>repair</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
-                                    <span className="fa fa-yelp" aria-hidden="true"></span>
-                                    <div className="read-btn">
-                                        <a href="about.html">view more</a>
-                                    </div>
-                                </div>
-                                <div className="banner-subg1">
-                                    <h3>improvement</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
-                                    <span className="fa fa-gg" aria-hidden="true"></span>
-                                    <div className="read-btn">
-                                        <a href="about.html">view more</a>
-                                    </div>
+                        <div className="col">
+                            <div className={styles.bannerSub1}>
+                                <h3>improvement</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
+                                <span className="fa fa-gg" aria-hidden="true"></span>
+                                <div className="read-btn">
+                                    <a href="about.html">view more</a>
                                 </div>
                             </div>
                         </div>
-                        <div className="clearfix"></div>
+                        <div className="col">
+                            <div className={styles.bannerSub1}>
+                                <h3>cleaning</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
+                                <span className="fa fa-check-square-o" aria-hidden="true"></span>
+                                <div className="read-btn">
+                                    <a href="about.html">view more</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className={styles.bannerSub1}>
+                                <h3>maintenance</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
+                                <span className="fa fa-cog" aria-hidden="true"></span>
+                                <div className="read-btn">
+                                    <a href="about.html">view more</a>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div className="row">
+
+                        <div className="col">
+                            <div className={styles.bannerSub1}>
+                                <h3>repair</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
+                                <span className="fa fa-yelp" aria-hidden="true"></span>
+                                <div className="read-btn">
+                                    <a href="about.html">view more</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className={styles.bannerSub1}>
+                                <h3>improvement</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
+                                <span className="fa fa-gg" aria-hidden="true"></span>
+                                <div className="read-btn">
+                                    <a href="about.html">view more</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className={styles.bannerSub1}>
+                                <h3>cleaning</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
+                                <span className="fa fa-check-square-o" aria-hidden="true"></span>
+                                <div className="read-btn">
+                                    <a href="about.html">view more</a>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
 		    </div>

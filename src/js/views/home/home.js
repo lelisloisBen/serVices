6ac4f4f76fdaca15a5ec356c938e21d51ad218ef
Samir Benzada <samirbenzada@gmail.com>
2020-03-18 14:58:32 -0400

    update

--- src/js/views/home/home.js
+++ src/js/views/home/home.js
@@ -8,49 +8,48 @@ import bg3 from '../../../img/home/bg3.jpg';
 const Home = () => {
     return (
         <div className={styles.main}>
-            <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src={bg1} class="d-block w-100" alt="bg1" />
+            <div id="carouselExampleFade" className="carousel slide carousel-fade" data-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={bg1} className="d-block w-100" alt="bg1" />
                     </div>
-                    <div class="carousel-item">
-                        <img src={bg2} class="d-block w-100" alt="bg2" />
+                    <div className="carousel-item">
+                        <img src={bg2} className="d-block w-100" alt="bg2" />
                     </div>
-                    <div class="carousel-item">
-                        <img src={bg3} class="d-block w-100" alt="bg3" />
+                    <div className="carousel-item">
+                        <img src={bg3} className="d-block w-100" alt="bg3" />
                     </div>
                 </div>
             </div>
 
-            <div class={styles.bannerServices}>
-                <div class="container">
-                    <div class="banner-bottom-main">
-                        <div class="col-md-4 banner-btmg1">
-
-                            <div class="form-text">
+            <div className={styles.bannerServices}>
+                <div className="container">
+                    <div className="row">
+                        <div className={["col-md-4", styles.bannerBox1].join(' ')}>
+                            <div className="form-text">
                                 <h3>Looking for a Handyman?</h3>
                                 <p>We solve your Home repair needs!</p>
                                 <img src="images/f1.png" alt="" />
                             </div>
-                            <form action="#" method="post" class="banner_form">
-                                <div class="sec-left">
-                                    <label class="contact-form-text">Name</label>
+                            <form action="#" method="post" className="banner_form">
+                                <div className="sec-left">
+                                    <label className="contact-form-text">Name</label>
                                     <input placeholder="your name " name="first name" type="text" required="" />
                                 </div>
-                                <div class="sec-right">
-                                    <label class="contact-form-text">Email</label>
+                                <div className="sec-right">
+                                    <label className="contact-form-text">Email</label>
                                     <input placeholder="xxx@xx.com " name="first name" type="email" required="" />
                                 </div>
-                                <div class="sec-left">
-                                    <label class="contact-form-text">Mobile no.</label>
+                                <div className="sec-left">
+                                    <label className="contact-form-text">Mobile no.</label>
                                     <input placeholder=" XXXXXX" name="first name" type="text" required="" />
                                 </div>
-                                <div class="form-tx">
-                                    <label class="contact-form-text">Address</label>
+                                <div className="form-tx">
+                                    <label className="contact-form-text">Address</label>
                                     <textarea placeholder="your address" required=""></textarea>
                                 </div>
-                                <div class="form-select sec-right">
-                                    <label class="contact-form-text">Select Service</label>
+                                <div className="form-select sec-right">
+                                    <label className="contact-form-text">Select Service</label>
                                     <select>
                                         <option value="0">---- SELECT ----</option>
                                         <option value="1">Electrical Service</option>
@@ -64,46 +63,46 @@ const Home = () => {
                                 <input type="submit" value="Submit" />
                             </form>
                         </div>
-                        <div class="col-md-8 banner-btm-grid2">
-                            <div class="col-md-4 banner-grid2">
-                                <div class="banner-subg1">
+                        <div className="col-md-8 banner-btm-grid2">
+                            <div className="col-md-4 banner-grid2">
+                                <div className="banner-subg1">
                                     <h3>maintenance</h3>
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
-                                    <span class="fa fa-cog" aria-hidden="true"></span>
-                                    <div class="read-btn">
+                                    <span className="fa fa-cog" aria-hidden="true"></span>
+                                    <div className="read-btn">
                                         <a href="about.html">view more</a>
                                     </div>
                                 </div>
-                                <div class="banner-subg1">
+                                <div className="banner-subg1">
                                     <h3>cleaning</h3>
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
-                                    <span class="fa fa-check-square-o" aria-hidden="true"></span>
-                                    <div class="read-btn">
+                                    <span className="fa fa-check-square-o" aria-hidden="true"></span>
+                                    <div className="read-btn">
                                         <a href="about.html">view more</a>
                                     </div>
                                 </div>
 
                             </div>
-                            <div class="col-md-4 banner-grid2">
-                                <div class="banner-subg1">
+                            <div className="col-md-4 banner-grid2">
+                                <div className="banner-subg1">
                                     <h3>repair</h3>
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
-                                    <span class="fa fa-yelp" aria-hidden="true"></span>
-                                    <div class="read-btn">
+                                    <span className="fa fa-yelp" aria-hidden="true"></span>
+                                    <div className="read-btn">
                                         <a href="about.html">view more</a>
                                     </div>
                                 </div>
-                                <div class="banner-subg1">
+                                <div className="banner-subg1">
                                     <h3>improvement</h3>
                                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit estibulum nibh urna scing.</p>
-                                    <span class="fa fa-gg" aria-hidden="true"></span>
-                                    <div class="read-btn">
+                                    <span className="fa fa-gg" aria-hidden="true"></span>
+                                    <div className="read-btn">
                                         <a href="about.html">view more</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="clearfix"></div>
+                        <div className="clearfix"></div>
                     </div>
                 </div>
 		    </div>

be48762a7ee75dff3fac2cf2716a8c703c7c6dfc
Samir Benzada <samirbenzada@gmail.com>
2020-03-18 14:00:18 -0400

    update

--- src/js/views/home/home.js
+++ src/js/views/home/home.js
@@ -1,10 +1,34 @@
-import React from 'react'
-import styles from './home.module.css'
+import React from 'react';
+import styles from './home.module.css';
+
+import bg1 from '../../../img/home/bg1.jpg';
+import bg2 from '../../../img/home/bg2.jpg';
+import bg3 from '../../../img/home/bg3.jpg';
 
 const Home = () => {
     return (
         <div className={styles.main}>
-            home page
+            <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src={bg1} class="d-block w-100" alt="bg1" />
+                    </div>
+                    <div class="carousel-item">
+                        <img src={bg2} class="d-block w-100" alt="bg2" />
+                    </div>
+                    <div class="carousel-item">
+                        <img src={bg3} class="d-block w-100" alt="bg3" />
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
         </div>
     );
 };

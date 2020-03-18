32aa68ff1ae0a7b9b14751ee5c84ed555ea3fbbc
Samir Benzada <samirbenzada@gmail.com>
2020-03-18 13:10:29 -0400

    update

---
+++ src/js/views/home/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './home.module.css'
+
+const Home = () => {
+    return (
+        <div className={styles.main}>
+            home page
+        </div>
+    );
+};
+
+export default Home;

6d67b3d99aee35198c2518d368a367368a75fe37
Samir Benzada <samirbenzada@gmail.com>
2020-03-18 14:47:40 -0400

    update

--- src/js/views/home/home.js
+++ src/js/views/home/home.js
@@ -20,14 +20,6 @@ const Home = () => {
                         <img src={bg3} class="d-block w-100" alt="bg3" />
                     </div>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
             </div>
         </div>
     );

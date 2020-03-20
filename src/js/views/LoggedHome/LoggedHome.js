eb7a78eea844224ce29c6da1af03bd9d019e7c5d
Samir Benzada <samirbenzada@gmail.com>
2020-03-20 11:43:51 -0400

    update

--- src/js/views/LoggedHome/LoggedHome.js
+++ src/js/views/LoggedHome/LoggedHome.js
@@ -13,20 +13,57 @@ const LoggedHome = () => {
                 <div className="container">
                     <h1>You are Logged Now</h1>
 
-                    <div className="col-md-4 col-sm-6 about-innergrids">
-                        <div className="agileits-news-gridtext">
-                            <div className="news-gridimg">
-                                <a href="single.html">
-                                    <img src={img} className="img-responsive zoom-img" alt="" />
-                                </a>
-                            </div>
-                            <div className="news-gridimgtext">
-                                <a href="single.html">
-                                    <h4>home Maintenance</h4>
-                                </a>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida mauris non mi gravida, at sollicitudin. </p>
+                    <div className="row">
+
+                        
+                        <div className={["col-md-4 col-sm-6", styles.cardMain].join(' ')}>
+                            <div className="agileits-news-gridtext">
+                                <div className="news-gridimg">
+                                    <a href="single.html">
+                                        <img src={img} className="img-responsive zoom-img" alt="" width="100%" />
+                                    </a>
+                                </div>
+                                <div className="news-gridimgtext">
+                                    <a href="single.html">
+                                        <h4>home Maintenance</h4>
+                                    </a>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida mauris non mi gravida, at sollicitudin. </p>
+                                </div>
                             </div>
                         </div>
+
+                        <div className={["col-md-4 col-sm-6", styles.cardMain].join(' ')}>
+                            <div className="agileits-news-gridtext">
+                                <div className="news-gridimg">
+                                    <a href="single.html">
+                                        <img src={img} className="img-responsive zoom-img" alt="" width="100%" />
+                                    </a>
+                                </div>
+                                <div className="news-gridimgtext">
+                                    <a href="single.html">
+                                        <h4>home Maintenance</h4>
+                                    </a>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida mauris non mi gravida, at sollicitudin. </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={["col-md-4 col-sm-6", styles.cardMain].join(' ')}>
+                            <div className="agileits-news-gridtext">
+                                <div className="news-gridimg">
+                                    <a href="single.html">
+                                        <img src={img} className="img-responsive zoom-img" alt="" width="100%" />
+                                    </a>
+                                </div>
+                                <div className="news-gridimgtext">
+                                    <a href="single.html">
+                                        <h4>home Maintenance</h4>
+                                    </a>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida mauris non mi gravida, at sollicitudin. </p>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>    
             </section>

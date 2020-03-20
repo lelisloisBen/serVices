21ebf3ad97fb47d9209a60d594992822c3eae828
Samir Benzada <samirbenzada@gmail.com>
2020-03-19 21:56:27 -0400

    update

--- src/js/views/home/home.js
+++ src/js/views/home/home.js
@@ -155,6 +155,15 @@ const Home = () => {
                 </div>
             </div>
 
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <img src="" alt=""/>
+                    </div>
+                    <div className="col-md-6"></div>
+                </div>
+            </div>
+
         </div>
     );
 };

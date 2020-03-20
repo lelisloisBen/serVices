c3cdb135d561ad3365f6ebc2afb0006fa2935733
Samir Benzada <samirbenzada@gmail.com>
2020-03-20 11:51:52 -0400

    update

--- src/js/views/LoggedHome/LoggedHome.js
+++ src/js/views/LoggedHome/LoggedHome.js
@@ -10,22 +10,17 @@ const LoggedHome = () => {
     return (
         <>
             <section className={styles.main}>
-                <div className="container">
-                    <h1>You are Logged Now</h1>
+                <div className="container mx-auto">
 
-                    <div className="row">
-
-                        
-                    <div class="card mb-3">
-                        <img src={img} class="card-img-top" alt="..." />
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        <div class="card mb-3">
+                            <img src={img} class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
                         </div>
-                    </div>
-
-                    </div>
+                        
                 </div>    
             </section>
         </>

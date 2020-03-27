93f5ac4e2e1af2665ca706c8aafa1c3004c480e2
Samir Benzada <samirbenzada@gmail.com>
2020-03-26 23:35:07 -0400

    update

--- src/js/views/Profile/Profile.js
+++ src/js/views/Profile/Profile.js
@@ -4,7 +4,17 @@ import styles from './Profile.module.css';
 const Profile = () => {
     return (
         <div className={["container", styles.main].join(' ')}>
-            profile page
+            <h1 className="text-center" >username</h1>
+
+            <label htmlFor="username">Username</label>
+            <br/>
+            &nbsp;
+            <input 
+                type="text" 
+                name="name" 
+                id="username"
+                placeholder="username"
+            />
         </div>
     );
 };

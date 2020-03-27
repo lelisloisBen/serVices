c645b0dad73bc9229fcf0991aaa44d76641bc7c9
Samir Benzada <samirbenzada@gmail.com>
2020-03-26 23:23:38 -0400

    update

---
+++ src/js/views/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Profile.module.css';
+
+const Profile = () => {
+    return (
+        <div className="main">
+            profile page
+        </div>
+    );
+};
+
+export default Profile; 

4e08531b7c45267b20e045f1f2ae7ef773941e53
Samir Benzada <samirbenzada@gmail.com>
2020-03-26 23:01:05 -0400

    update

--- src/js/views/Register/Register.js
+++ src/js/views/Register/Register.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import styles from './Register.module.css';
 
+const Register = () => {
+    return (
+        <div className={styles.main}>
+            <h1>Register Page</h1>
+        </div>
+    );
+};
+
+export default Register;

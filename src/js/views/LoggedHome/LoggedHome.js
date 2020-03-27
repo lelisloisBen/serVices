bcee0431dccf9d9313e36c99796aa2fa6a406d48
Samir Benzada <samirbenzada@gmail.com>
2020-03-27 00:02:16 -0400

    update

--- src/js/views/LoggedHome/LoggedHome.js
+++ src/js/views/LoggedHome/LoggedHome.js
@@ -15,88 +15,88 @@ const LoggedHome = () => {
             <section className={styles.main}>
                 <div className="container mx-auto">
 
-                        <div class="card mb-3">
-                            <img src={img} class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Mila</h5>
-                                <p class="card-text">This is a card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text">
-                                    <small class="text-muted">
-                                        <i class="fas fa-fist-raised"></i>$50-30min
+                        <div className="card mb-3">
+                            <img src={img} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Mila</h5>
+                                <p className="card-text">This is a card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p className="card-text">
+                                    <small className="text-muted">
+                                        <i className="fas fa-fist-raised"></i>$50-30min
                                         &nbsp;/&nbsp;
-                                        <i class="far fa-surprise"></i> $100-30min
+                                        <i className="far fa-surprise"></i> $100-30min
                                         &nbsp;/&nbsp;
-                                        <i class="fas fa-smile"></i>$300-1h
+                                        <i className="fas fa-smile"></i>$300-1h
                                     </small>
                                     <br/>
-                                    <small class="text-muted">
-                                        <i class="fab fa-instagram"></i>
+                                    <small className="text-muted">
+                                        <i className="fab fa-instagram"></i>
                                         &nbsp;
                                         Mila
                                     </small>
                                 </p>
                             </div>
                         </div>
-                        <div class="card mb-3">
-                            <img src={img2} class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Anastasia</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text">
-                                    <small class="text-muted">
-                                        <i class="fas fa-fist-raised"></i>$50-30min
+                        <div className="card mb-3">
+                            <img src={img2} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Anastasia</h5>
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p className="card-text">
+                                    <small className="text-muted">
+                                        <i className="fas fa-fist-raised"></i>$50-30min
                                         &nbsp;/&nbsp;
-                                        <i class="far fa-surprise"></i> $100-30min
+                                        <i className="far fa-surprise"></i> $100-30min
                                         &nbsp;/&nbsp;
-                                        <i class="fas fa-smile"></i>$300-1h
+                                        <i className="fas fa-smile"></i>$300-1h
                                     </small>
                                     <br/>
-                                    <small class="text-muted">
-                                        <i class="fab fa-instagram"></i>
+                                    <small className="text-muted">
+                                        <i className="fab fa-instagram"></i>
                                         &nbsp;
                                         Anastasia
                                     </small>
                                 </p>
                             </div>
                         </div>
-                        <div class="card mb-3">
-                            <img src={img3} class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Lia</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text">
-                                    <small class="text-muted">
-                                        <i class="fas fa-fist-raised"></i>$50-30min
+                        <div className="card mb-3">
+                            <img src={img3} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Lia</h5>
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p className="card-text">
+                                    <small className="text-muted">
+                                        <i className="fas fa-fist-raised"></i>$50-30min
                                         &nbsp;/&nbsp;
-                                        <i class="far fa-surprise"></i> $100-30min
+                                        <i className="far fa-surprise"></i> $100-30min
                                         &nbsp;/&nbsp;
-                                        <i class="fas fa-smile"></i>$300-1h
+                                        <i className="fas fa-smile"></i>$300-1h
                                     </small>
                                     <br/>
-                                    <small class="text-muted">
-                                        <i class="fab fa-instagram"></i>
+                                    <small className="text-muted">
+                                        <i className="fab fa-instagram"></i>
                                         &nbsp;
                                         Lia
                                     </small>
                                 </p>
                             </div>
                         </div>
-                        <div class="card mb-3">
-                            <img src={img4} class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Nina</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text">
-                                    <small class="text-muted">
-                                        <i class="fas fa-fist-raised"></i>$50-30min
+                        <div className="card mb-3">
+                            <img src={img4} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">Nina</h5>
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p className="card-text">
+                                    <small className="text-muted">
+                                        <i className="fas fa-fist-raised"></i>$50-30min
                                         &nbsp;/&nbsp;
-                                        <i class="far fa-surprise"></i> $100-30min
+                                        <i className="far fa-surprise"></i> $100-30min
                                         &nbsp;/&nbsp;
-                                        <i class="fas fa-smile"></i>$300-1h
+                                        <i className="fas fa-smile"></i>$300-1h
                                     </small>
                                     <br/>
-                                    <small class="text-muted">
-                                        <i class="fab fa-instagram"></i>
+                                    <small className="text-muted">
+                                        <i className="fab fa-instagram"></i>
                                         &nbsp;
                                         Nina
                                     </small>
